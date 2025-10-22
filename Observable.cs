562cd4b9255634f9324308daa130ca0a53787d10
Iago Fonteles <iagofonteles1@gmail.com>
2025-10-22 20:07:13 -0300

    first commit

---
+++ Observable.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BlueGravity
+{
+    public delegate void ValueChangedHandler<in T>(T value);
+
+    public delegate void ValueChangedHandler2<in T>(T oldValue, T newValue);
+
+    public interface IReadOnlyObservable
+    {
+        object Value { get; }
+        event ValueChangedHandler<object> OnChanged;
+    }
+
+    public interface IObservable : IReadOnlyObservable
+    {
+        new object Value { get; set; }
+        object IReadOnlyObservable.Value => Value;
+    }
+
+    public interface IReadOnlyObservable<out T> : IReadOnlyObservable
+    {
+        object IReadOnlyObservable.Value => Value;
+        public new T Value { get; }
+        public event ValueChangedHandler<T> OnValueChanged;
+        public event ValueChangedHandler2<T> OnValueChanged2;
+    }
+
+    [Serializable]
+    public class Observable<T> : IObservable, IReadOnlyObservable<T>
+    {
+        [SerializeField] private T value;
+
+        public Observable() { }
+
+        public Observable(T value) => this.value = value;
+
+        object IObservable.Value { get => value; set => Value = value is T v ? v : default; }
+
+        public T Value
+        {
+            get => value;
+            set
+            {
+                if (EqualityComparer<T>.Default.Equals(value, this.value))
+                    return;
+
+                var old = this.value;
+                this.value = value;
+                OnChanged?.Invoke(value);
+                OnValueChanged?.Invoke(value);
+                OnValueChanged2?.Invoke(old, value);
+            }
+        }
+
+        public void SetSilently(T val) => value = val;
+        public event ValueChangedHandler<object> OnChanged;
+        public event ValueChangedHandler<T> OnValueChanged;
+        public event ValueChangedHandler2<T> OnValueChanged2;
+        object IReadOnlyObservable.Value => Value;
+        public override string ToString() => $"({typeof(T).Name}) {value}";
+    }
+}
---
+++ ObservableList.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+namespace Utility
+{
+    using Action = NotifyCollectionChangedAction;
+    using ChangedArgs = NotifyCollectionChangedEventArgs;
+
+    public interface IObservableList<out T> : IReadOnlyList<T>, INotifyCollectionChanged { }
+
+    /// <summary>
+    /// Made this cause ObservableCollection inst serializable
+    /// </summary>
+    [Serializable]
+    public class ObservableList<T> : IList<T>, IObservableList<T>
+    {
+        [SerializeField] private List<T> list;
+
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+
+        public T this[int index] { get => list[index]; set => list[index] = value; }
+
+        public int Count => list.Count;
+        public bool IsReadOnly => ((IList<T>)list).IsReadOnly;
+
+        public bool Contains(T item) => list.Contains(item);
+        public int IndexOf(T item) => list.IndexOf(item);
+        public int FindIndex(Predicate<T> predicate) => list.FindIndex(predicate);
+        public void CopyTo(T[] array, int arrayIndex) => list.CopyTo(array, arrayIndex);
+        public void Add(T item) => Insert(Count, item);
+
+        public void AddRange(IEnumerable<T> items)
+        {
+            foreach (var item in items) Add(item);
+        }
+
+        public IEnumerator<T> GetEnumerator() => list.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)list).GetEnumerator();
+
+        public ObservableList() => list = new();
+        public ObservableList(IEnumerable<T> items) => list = new(items);
+
+        public void Insert(int index, T item)
+        {
+            list.Insert(index, item);
+            var args = new ChangedArgs(Action.Add, item, index);
+            CollectionChanged?.Invoke(this, args);
+        }
+
+        public void Clear()
+        {
+            list.Clear();
+            var args = new ChangedArgs(Action.Reset);
+            CollectionChanged?.Invoke(this, args);
+        }
+
+        public bool Remove(T item)
+        {
+            var index = list.IndexOf(item);
+            if (index < 0) return false;
+            RemoveAt(index);
+            return true;
+        }
+
+        public void RemoveAt(int index)
+        {
+            var item = list[index];
+            list.RemoveAt(index);
+            var args = new ChangedArgs(Action.Remove, item, index);
+            CollectionChanged?.Invoke(this, args);
+        }
+
+        public void RemoveAll(Func<T, bool> predicate)
+        {
+            for (var i = list.Count - 1; i >= 0; i--)
+                if (predicate(list[i]))
+                    RemoveAt(i);
+        }
+    }
+}
