5af18cc4db3ea4496c2d0e232471858bf1504018
Iago Fonteles <iagofonteles1@gmail.com>
2026-03-07 20:15:53 -0300

    observable array

---
+++ Runtime/ObservableArray.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+namespace Drafts
+{
+    using Action = NotifyCollectionChangedAction;
+    using ChangedArgs = NotifyCollectionChangedEventArgs;
+    
+    public delegate void ArrayEventHandler<in T>(int index, T value);
+
+    public interface IObservableArray<out T> : IReadOnlyList<T>, INotifyCollectionChanged
+    {
+        event ArrayEventHandler<T> OnChanged;
+    }
+
+    /// <summary>
+    /// Fixed-size observable collection that wraps an array.
+    /// </summary>
+    [Serializable]
+    public class ObservableArray<T> : IObservableArray<T>, IList<T>
+    {
+        [SerializeField] private T[] array;
+
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+        public event ArrayEventHandler<T> OnChanged;
+
+        public T this[int index]
+        {
+            get => array[index];
+            set
+            {
+                var old = array[index];
+                array[index] = value;
+                var args = new ChangedArgs(Action.Replace, value, old, index);
+                CollectionChanged?.Invoke(this, args);
+                OnChanged?.Invoke(index, value);
+            } 
+        }
+
+        public int Count => array.Length;
+        public bool IsReadOnly => array.IsReadOnly;
+
+        public bool Contains(T item) => Array.IndexOf(array, item) >= 0;
+        public int IndexOf(T item) => Array.IndexOf(array, item);
+        public void CopyTo(T[] array, int arrayIndex) => this.array.CopyTo(array, arrayIndex);
+
+        public IEnumerator<T> GetEnumerator() => ((IEnumerable<T>)array).GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => array.GetEnumerator();
+
+        public ObservableArray(int size) => array = new T[size];
+        public ObservableArray(T[] initialArray) => array = initialArray;
+
+        public void Add(T item) => throw new NotSupportedException("ObservableArray has a fixed size.");
+        public void Insert(int index, T item) => throw new NotSupportedException("ObservableArray has a fixed size.");
+        public bool Remove(T item) => throw new NotSupportedException("ObservableArray has a fixed size.");
+        public void RemoveAt(int index) => throw new NotSupportedException("ObservableArray has a fixed size.");
+        public void Clear() => throw new NotSupportedException("ObservableArray has a fixed size.");
+    }
+}
